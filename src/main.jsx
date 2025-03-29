413cbd59356dd4a68aeb46c12703bbee9b939604
RohanLakade <lakade.rohan419@gmail.com>
2025-03-29 15:25:35 +0530

    scss  fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "@src/App";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "@context/AuthContext";
-import "@styles/index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "@styles/index.scss";
 
 import { ProductProvider } from "./context/ProductContext";
 
